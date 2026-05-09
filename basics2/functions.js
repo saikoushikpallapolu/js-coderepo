dd03e5c4cd82ffd89f6fc2ce07a0e80083e52645
saikoushikpallapolu <saikoushikpallapolu@gmail.com>
2026-05-09 19:58:32 +0000

    goodnight(9may)

--- basics2/functions.js
+++ basics2/functions.js
@@ -15,4 +15,19 @@ function cart(...number){ // REST operator, when the no of parameters is not kno
 
 console.log(cart(100,200,300,400)); // prints an array of these arguments.
 
+const addtwo = function(n1){
+    return n1+2
+} // another way of creating a function.
 
+const answer = addtwo(4)
+console.log(answer)
+
+addthree(5)
+
+function addthree(nu1){
+    console.log(nu1+3);
+} // still executes eventhough called before.
+
+const addthree = function(num1){
+    console.log(num1+3);
+} // doesnt work as called before the declaration.
